50bac6f85a1467a9c7a30e8da13339c6a4eec970
Santos <santicardonal777@icloud.com>
2025-12-23 14:00:07 -0500

    Clase 67: manejo de evento jsx

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,12 @@
 export function Guitar({guitar}) {
 
-    const {name, price, description, image} = guitar
+    const {id, name, price, description, image} = guitar
+
+    const handleClick = (id) => {
+
+        console.log('Diste click ', id);
+        
+    }
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -14,6 +20,7 @@ export function Guitar({guitar}) {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={()=>  handleClick(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
