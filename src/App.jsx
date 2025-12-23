0aa1d0c30032af80ff94e6b1ad6d7a14aef61c66
Santos <santicardonal777@icloud.com>
2025-12-23 12:56:16 -0500

    se agrego la data a Guitar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
 import Header from "./components/Header.jsx";
-import { Guitar } from "./components/Guitar.jsx";
-import { useEffect, useState } from "react";
+import {Guitar} from "./components/Guitar.jsx";
+import { useState } from "react";
 import { db } from "./data/db.js";
 
-
-
 function App() {
+
+
   //state
-
-  const[data, setData] = useState(db)
-
-
+  const [data, setData] = useState(db)
+  
+  
 
   return (
     <>
 
       <Header></Header>
+      
 
 
-
-      <main className="container-xl mt-5">
+    <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
 
           {
-            
-            data.map((guitar)=> (
+            data.map((guitar) => (
 
-              <Guitar
-                key={guitar.id} 
+              <Guitar 
+                key={guitar.id}
                 guitar={guitar}
+              
               />
-
             ))
+            
           }
 
+
         </div>
-      </main>
+    </main>
 
 
-      <footer className="bg-dark mt-5 py-5">
+    <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
-      </footer>
+    </footer>
 
-
+      
     </>
   )
 }
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,6 @@
 export function Guitar({guitar}) {
 
-    const {id, name, price, image, description, } = guitar
-    
-    
-
+    const {name, price, description, image} = guitar
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -11,13 +8,12 @@ export function Guitar({guitar}) {
                 <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
             </div>
             <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
                 <p>{description}</p>
-                <p className="fw-black text-primary fs-3">${price}</p>
+                <p className="fw-black text-primary fs-3">$299</p>
                 <button
                     type="button"
                     className="btn btn-dark w-100"
-                    
                 >Agregar al Carrito</button>
             </div>
         </div>
